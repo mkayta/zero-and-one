b8849959792fbe91088e0adac2de0d2dfa32ea3a
Eng-Mohamed <mkayta2023@gmail.com>
2024-10-02 17:52:09 +0300

    I done this Challenge

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // Good Luck 💪🏾
+function ZeroAndOne(num){
+    return num 
+    .filter(item =>item=== "Zero"|| item==="ONE")
+    .map(item =>(item==="Zero"?"0":"1"))
+    .join(" ");
+
+}
+ console.log(ZeroAndOne(["one","Zero","Zero","One"])) 
+console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"])) 
